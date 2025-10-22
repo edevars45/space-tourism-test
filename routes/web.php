2878676ebb35daf14c7df493a8439cc595e9e528
Esther <devarsesther@gmail.com>
2025-10-22 14:18:22 +0200

    Auth: install Laravel Breeze (Blade) + migrations

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
-    return view('vue.accueil');
-})->name('accueil');
+    return view('welcome');
+});
 
-Route::get('destination', function () {
-    return view('vue.destination');
-})->name('destination');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('equipage', function () {
-    return view('vue.equipage');
-})->name('equipage');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('technologie', function () {
-    return view('vue.technologie');
-})->name('technologie');
-
-
-Route::get('lang/{locale}', function ($locale) {
-    if (in_array($locale, ['fr', 'en'])) {
-        Session::put('locale', $locale);
-    }
-    return redirect()->back();});
+require __DIR__.'/auth.php';
